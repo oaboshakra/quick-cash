08e90a4786ad1c3d69ef2629c9e481b7599cb1f0
Nithish Thirunavukkarasu <nt477834@dal.ca>
2024-02-03 18:07:33 -0400

    Espresso test implemented for Login activity and test cases are failing succesfully

--- QuickCash/app/src/androidTest/java/com/example/quickcash/LoginEspressoTest.java
+++ QuickCash/app/src/androidTest/java/com/example/quickcash/LoginEspressoTest.java
@@ -1,12 +1,101 @@
 package com.example.quickcash;
-import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.core.app.ActivityScenario;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.Espresso;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+
+
+import com.example.quickcash.ui.Authentication.LoginActivity;
+import com.example.quickcash.util.AppConstants;
+
+
 public class LoginEspressoTest {
 
+    ActivityScenario<LoginActivity> activityScenario;
+
+    @Before
+    public void setUp(){
+        activityScenario = ActivityScenario.launch(LoginActivity.class);
+        activityScenario.onActivity(activity -> {
+
+        });
+    }
+    @Test
+    public void testInvalidEmail() {
+         onView(withId(R.id.Sign_In_Email)).perform(typeText(AppConstants.INVALID_EMAIL));
+         onView(withId(R.id.Sign_In_Password)).perform(typeText(AppConstants.VALID_PASSWORD));
+         onView(withId(R.id.Sign_In_Request)).perform(click());
+         onView(withId(R.id.statusLabel)).check(matches(withText(AppConstants.INVALID_EMAIL_ERROR)));
+    }
+    @Test
+    public void testInvalidPassword(){
+        onView(withId(R.id.Sign_In_Email)).perform(typeText(AppConstants.VALID_PASSWORD));
+        onView(withId(R.id.Sign_In_Password)).perform(typeText(AppConstants.INVALID_PASSWORD));
+        onView(withId(R.id.Sign_In_Request)).perform(click());
+        onView(withId(R.id.statusLabel)).check(matches(withText(AppConstants.INVALID_PASSWORD_ERROR)));
+    }
+
+    @Test
+    public void testEmptyPassword(){
+        onView(withId(R.id.Sign_In_Email)).perform(typeText(AppConstants.VALID_PASSWORD));
+        onView(withId(R.id.Sign_In_Password)).perform(typeText(AppConstants.EMPTY_STRING));
+        onView(withId(R.id.Sign_In_Request)).perform(click());
+        onView(withId(R.id.statusLabel)).check(matches(withText(AppConstants.FIELD_EMPTY_ERROR)));
+    }
+
+
+    @Test
+    public void testEmptyEmail(){
+        onView(withId(R.id.Sign_In_Email)).perform(typeText(AppConstants.EMPTY_STRING));
+        onView(withId(R.id.Sign_In_Password)).perform(typeText(AppConstants.VALID_PASSWORD));
+        onView(withId(R.id.Sign_In_Request)).perform(click());
+        onView(withId(R.id.statusLabel)).check(matches(withText(AppConstants.FIELD_EMPTY_ERROR)));
+    }
+
+    @Test
+    public void testEmailPasswordEmpty(){
+        onView(withId(R.id.Sign_In_Email)).perform(typeText(AppConstants.EMPTY_STRING));
+        onView(withId(R.id.Sign_In_Password)).perform(typeText(AppConstants.EMPTY_STRING));
+        onView(withId(R.id.Sign_In_Request)).perform(click());
+        onView(withId(R.id.statusLabel)).check(matches(withText(AppConstants.FIELD_EMPTY_ERROR)));
+    }
+
+    @Test
+    public void testValidPasswordEmail(){
+        onView(withId(R.id.Sign_In_Email)).perform(typeText(AppConstants.VALID_EMAIL));
+        onView(withId(R.id.Sign_In_Password)).perform(typeText(AppConstants.VALID_PASSWORD));
+        onView(withId(R.id.Sign_In_Request)).perform(click());
+        onView(withId(R.id.statusLabel)).check(matches(withText(AppConstants.EMPTY_STRING)));
+
+    }
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
--- QuickCash/app/src/main/java/com/example/quickcash/util/AppConstants.java
+++ QuickCash/app/src/main/java/com/example/quickcash/util/AppConstants.java
@@ -1,8 +1,26 @@
 package com.example.quickcash.util;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+
+import com.example.quickcash.ui.Authentication.LoginActivity;
+
 public class AppConstants {
 
     public static  final String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
     public static final int PASSWORD_LENGTH = 8;
 
+    public static final Class<LoginActivity> LAUNCHER_CLASS= LoginActivity.class;
+
+    public static final String INVALID_EMAIL = "inavlid1004";
+    public static final String VALID_EMAIL = "nith2411@gmail.com";
+
+    public static final String VALID_PASSWORD ="abcdefgh" ;
+    public static final String INVALID_PASSWORD = "123";
+    public static final String INVALID_EMAIL_ERROR = "EMAIL_INAVLID" ;
+    public static final String INVALID_PASSWORD_ERROR = "PASSWORD_INVALID";
+
+    public static final String EMPTY_STRING = "";
+    public static final String FIELD_EMPTY_ERROR = "Required fields cannot be empty";
 }
