ccf4985692c57d2ae234afba05029238fd6318c4
Cheng Zheng <ch275214.com>
2024-02-15 14:47:21 -0400

    fix merge conflict

--- QuickCash/app/src/main/java/com/example/quickcash/ui/Authentication/ApplyJobActivity.java
+++ QuickCash/app/src/main/java/com/example/quickcash/ui/Authentication/ApplyJobActivity.java
@@ -1,15 +1,99 @@
 package com.example.quickcash.ui.Authentication;
 
+import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 //import com.example.quickcash.adapter.Authentication;
 import com.example.quickcash.R;
+import com.example.quickcash.databinding.ActivityLoginBinding;
+import com.example.quickcash.util.AppConstants;
+import com.example.quickcash.util.DataValidator;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ApplyJobActivity extends AppCompatActivity {
 
-    Button applyBtn = findViewById(R.id.apply_btn);
+    private ActivityLoginBinding binding;
+    private FirebaseAuth auth;
+    private String errorMessage = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Button applyBtn = findViewById(R.id.apply_btn);
+        applyBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                EditText nameTextBox = findViewById(R.id.apply_Name);
+                EditText phoneTextBox = findViewById(R.id.apply_Phone);
+                EditText emailTextBox = findViewById(R.id.apply_Email);
+
+                TextView statusLabel = findViewById(R.id.statusLabel);
+                setErrorMessage(AppConstants.EMPTY_STRING);
+
+                String name = nameTextBox.getText().toString();
+                String phone = phoneTextBox.getText().toString();
+                String email = emailTextBox.getText().toString();
 
 
+                boolean ready = true;
+                if (email.equals(AppConstants.EMPTY_STRING) || name.equals(AppConstants.EMPTY_STRING)|| phone.equals(AppConstants.EMPTY_STRING)) {
+                    setErrorMessage(AppConstants.FIELD_EMPTY_MESSAGE);
+                    statusLabel.setText(errorMessage);
+                    Toast.makeText(ApplyJobActivity.this, errorMessage ,Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (ready && !DataValidator.isValidEmail(email)) {
+                    setErrorMessage(AppConstants.INVALID_EMAIL_MESSAGE);
+                    statusLabel.setText(errorMessage);
+                    Toast.makeText(ApplyJobActivity.this, errorMessage ,Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (ready && !DataValidator.isValidPassword(name)) {
+                    setErrorMessage(AppConstants.INVALID_NAME_MESSAGE);
+                    statusLabel.setText(errorMessage);
+                    Toast.makeText(ApplyJobActivity.this, errorMessage ,Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (ready && !DataValidator.isValidPassword(phone)) {
+                    setErrorMessage(AppConstants.INVALID_PHONE_MESSAGE);
+                    statusLabel.setText(errorMessage);
+                    Toast.makeText(ApplyJobActivity.this, errorMessage ,Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                statusLabel.setText(AppConstants.EMPTY_STRING);
+
+
+                FirebaseApp.initializeApp(ApplyJobActivity.this);
+                auth = FirebaseAuth.getInstance();
+                if(ready) {
+
+                }
+
+                Toast.makeText(ApplyJobActivity.this, errorMessage ,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
 }
