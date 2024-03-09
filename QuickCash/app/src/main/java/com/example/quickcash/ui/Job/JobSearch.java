c5068833c077d113d24d3be60e1a5da211a2b76b
Cheng Zheng <ch275214.com>
2024-03-08 22:33:54 -0400

    Add jobName for FIREBASE and back btn

--- QuickCash/app/src/main/java/com/example/quickcash/ui/Job/JobSearch.java
+++ QuickCash/app/src/main/java/com/example/quickcash/ui/Job/JobSearch.java
@@ -82,6 +82,14 @@ public class JobSearch extends AppCompatActivity {
                             TimeZone.setText(String.format("TimeZone: %s", job.getTimeZone()));
                             Wage.setText(String.format("Wage: %s", job.getWage()));
                             JobOwner.setText(String.format("JobOwner: %s", job.getJobOwner()));
+                            apply.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Intent intent = new Intent(getApplicationContext(), ApplyJobActivity.class);
+                                    intent.putExtra("Job", job.getName());
+                                    startActivity(intent);
+                                }
+                            });
                         }
                         break;
                     }
@@ -90,14 +98,6 @@ public class JobSearch extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 Log.e("SearchActivity", error.getMessage());
-            }
-        });
-
-        apply.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ApplyJobActivity.class);
-                startActivity(intent);
             }
         });
     }
