e0509f5e4187aa05349b6b204136b636a6fc15ea
oaboshakra <os725191@dal.ca>
2024-03-16 15:43:37 -0300

    Added some espresso tests for the ProfileSuggestion class.

--- QuickCash/app/src/androidTest/java/com/example/quickcash/ProfileSuggestionTest.java
+++ QuickCash/app/src/androidTest/java/com/example/quickcash/ProfileSuggestionTest.java
@@ -1,4 +1,27 @@
 package com.example.quickcash;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.core.app.ActivityScenario;
+import com.example.quickcash.ui.ProfileSuggestion.ProfileSuggestion;
+import org.junit.Before;
+import org.junit.Test;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import com.example.quickcash.util.AppConstants;
 
 public class ProfileSuggestionTest {
+    @Before
+    public void launchActivity() {
+        ActivityScenario.launch(ProfileSuggestion.class);
+    }
+
+    public void testNameDisplayed() {
+        onView(withId(R.id.profileName)).check(matches(withText(AppConstants.Name)));
+    }
+    public void testTimeZoneDisplayed() {
+        onView(withId(R.id.profileEmail)).check(matches(withText(AppConstants.VALID_EMAIL)));
+    }
 }
