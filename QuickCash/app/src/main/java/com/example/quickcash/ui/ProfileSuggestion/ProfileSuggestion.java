26659e8c9d7aa61ed707718dde5116a84c4cd181
oaboshakra <os725191@dal.ca>
2024-03-16 17:39:56 -0300

    Created the UI and necessary xml files for the ProfileSuggestion feature.

--- QuickCash/app/src/main/java/com/example/quickcash/ui/ProfileSuggestion/ProfileSuggestion.java
+++ QuickCash/app/src/main/java/com/example/quickcash/ui/ProfileSuggestion/ProfileSuggestion.java
@@ -1,4 +1,13 @@
 package com.example.quickcash.ui.ProfileSuggestion;
+import com.example.quickcash.R;
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class ProfileSuggestion {
+public class ProfileSuggestion extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_suggestion);
+    }
 }
