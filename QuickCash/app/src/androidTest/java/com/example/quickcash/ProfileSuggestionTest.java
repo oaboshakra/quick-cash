feccc38803ca130e51a1fa289f739e92d776510b
oaboshakra <os725191@dal.ca>
2024-03-17 17:39:11 -0300

    Did some even more refactoring. Code is more effecient and looks better.

--- QuickCash/app/src/androidTest/java/com/example/quickcash/ProfileSuggestionTest.java
+++ QuickCash/app/src/androidTest/java/com/example/quickcash/ProfileSuggestionTest.java
@@ -1,19 +1,13 @@
 package com.example.quickcash;
-import androidx.test.core.app.ActivityScenario;
 import com.example.quickcash.ui.ProfileSuggestion.ProfileSuggestion;
-import org.junit.Before;
 import org.junit.Test;
 
-import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.Espresso;
-import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.LargeTest;
 import org.junit.Rule;
-import org.junit.Test;
-import com.example.quickcash.R;
 
 @LargeTest
 public class ProfileSuggestionTest {
